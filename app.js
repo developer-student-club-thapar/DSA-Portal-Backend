68efa547ee6dca7ce7359dbda590eda294fc8884
Manan Sharma <112090668+Manan-Sharma-5@users.noreply.github.com>
2023-09-17 15:57:24 +0530

    added cors and updated login controller

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-require('dotenv').config()
-const express = require('express')
-const dbConfig = require('./config/dbConfig')
-const authRoute = require('./routes/authRoute')
-const userRoute = require('./routes/userRoute')
-const auth = require('./middleware/auth')
-const cookieParser = require('cookie-parser')
+require("dotenv").config();
+const express = require("express");
+const dbConfig = require("./config/dbConfig");
+const authRoute = require("./routes/authRoute");
+const userRoute = require("./routes/userRoute");
+const auth = require("./middleware/auth");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const server = express()
+const server = express();
 
 // connect to db
-dbConfig()
+dbConfig();
 
 // middlewares
-server.use(express.json())
-server.use(cookieParser())
+server.use(express.json());
+server.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+server.use(cookieParser());
 
 // routes
-server.use('/api/auth', authRoute)
-server.use('/api/user', auth, userRoute)
+server.use("/api/auth", authRoute);
+server.use("/api/user", auth, userRoute);
 
-module.exports = server
+module.exports = server;
