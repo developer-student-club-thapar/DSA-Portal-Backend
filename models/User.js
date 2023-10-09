dc0168d5f56f51917412996dccb1b2e0f5bf8d55
Manan Sharma <112090668+Manan-Sharma-5@users.noreply.github.com>
2023-10-09 11:50:27 +0530

    Made changes to correct the leaderboard updation

--- models/User.js
+++ models/User.js
@@ -1,34 +1,34 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique:true,
-    },
-    leetcodeUserName: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    leetcodeCookies: {
-        type: String,
-        required: true
-    },
-    solvedProblems: {
-        type: [String],
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  leetcodeUserName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  leetcodeCookies: {
+    type: String,
+    required: true,
+  },
+  solvedProblems: {
+    type: Array,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
